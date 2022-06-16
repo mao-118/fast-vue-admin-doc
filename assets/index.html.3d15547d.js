a538a5b348382fe2abbb36c4b8fb1ff713c5846f
mao-118 <>
2022-06-16 09:38:12 +0000

    Deploying to gh-pages from @ mao-118/fast-vue-admin-doc@d3077278db0aed1e86fff545841d272738f6601a 🚀

---
+++ assets/index.html.3d15547d.js
@@ -0,0 +1,99 @@
+import{_ as o,r,o as a,c as l,a as n,b as i,F as d,e,d as t}from"./app.0ebc1139.js";var c="/fast-vue-admin-doc/images/vue-version.svg",v="/fast-vue-admin-doc/images/dashboard.png";const u={},_=n("h1",{id:"\u4ECB\u7ECD",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u4ECB\u7ECD","aria-hidden":"true"},"#"),e(" \u4ECB\u7ECD")],-1),h={href:"https://github.com/vuejs/vue",target:"_blank",rel:"noopener noreferrer"},m=n("img",{src:c,alt:"vue"},null,-1),p=n("br",null,null,-1),b=e(" \u672C\u9879\u76EE\u57FA\u4E8E "),g={href:"https://staging-cn.vuejs.org/guide/introduction.html",target:"_blank",rel:"noopener noreferrer"},f=e("vue3"),k=e(" \u548C "),j={href:"https://vitejs.cn/",target:"_blank",rel:"noopener noreferrer"},x=e("vite2"),V=e(" \u642D\u5EFA,\u81EA2022\u5E74\u5F00\u59CB "),S={href:"https://staging-cn.vuejs.org/guide/introduction.html",target:"_blank",rel:"noopener noreferrer"},w=e("vue3"),C=e(" \u5DF2\u7ECF\u5F00\u59CB\u4F5C\u4E3A\u9ED8\u8BA4\u7248\u672C\u3002\u4E0D\u5728\u5EFA\u8BAE\u4F7F\u7528 "),I={href:"https://cn.vuejs.org/",target:"_blank",rel:"noopener noreferrer"},P=e("vue2"),y=e(" \u7684\u6846\u67B6\uFF0C "),E={href:"https://www.webpackjs.com/",target:"_blank",rel:"noopener noreferrer"},A=e("webpack"),N=e(" \u6784\u5EFA\u5DE5\u5177\u4E5F\u66FF\u6362\u4E3A "),M={href:"https://vitejs.cn/",target:"_blank",rel:"noopener noreferrer"},R=e("vite"),z=e(" \uFF0C\u540C\u65F6\u8BE5\u9879\u76EE\u5168\u5C40\u72B6\u6001\u5171\u4EAB\u4ECE\u4E4B\u524D\u7684 "),D={href:"https://vuex.vuejs.org/",target:"_blank",rel:"noopener noreferrer"},T=e("Vuex"),B=e(" \u66FF\u6362\u4E3A "),F={href:"https://pinia.vuejs.org/introduction.html",target:"_blank",rel:"noopener noreferrer"},H=e("pinia"),J=n("h2",{id:"\u524D\u5E8F\u51C6\u5907",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u524D\u5E8F\u51C6\u5907","aria-hidden":"true"},"#"),e(" \u524D\u5E8F\u51C6\u5907")],-1),L=e("\u4F60\u9700\u8981\u5728\u672C\u5730\u5B89\u88C5 "),Q={href:"http://nodejs.org/",target:"_blank",rel:"noopener noreferrer"},O=e("node"),q=e(" \u548C "),G={href:"https://git-scm.com/",target:"_blank",rel:"noopener noreferrer"},K=e("git"),U=e("\u3002"),W=e("\u5E0C\u671B\u4F60\u5177\u5907 "),X={href:"https://cn.vuejs.org/",target:"_blank",rel:"noopener noreferrer"},Y=e("vue2"),Z=e(" \u7684\u5E38\u7528\u77E5\u8BC6\uFF0C\u8FD9\u6837\u53EF\u4EE5\u76F4\u63A5\u8FC7\u6E21\u5230 "),$={href:"https://staging-cn.vuejs.org/guide/introduction.html",target:"_blank",rel:"noopener noreferrer"},ee=e("vue3"),ne=e(" \u3002\u5982\u679C\u6CA1\u6709\u4E5F\u65E0\u5173\u7D27\u8981\uFF0C\u53EF\u4EE5\u76F4\u63A5\u4ECE "),se={href:"https://staging-cn.vuejs.org/guide/introduction.html",target:"_blank",rel:"noopener noreferrer"},ie=e("vue3"),te=e(" \u5F00\u59CB\u5B66\u4E60\uFF0C\u6BD5\u7ADF "),oe={href:"https://staging-cn.vuejs.org/guide/introduction.html",target:"_blank",rel:"noopener noreferrer"},re=e("vue3"),ae=e(" \u5DF2\u7ECF\u5F00\u59CB\u4E3A\u4E3B\u6D41\u7248\u672C\u3002"),le=e("\u638C\u63E1 "),de={href:"https://es6.ruanyifeng.com/",target:"_blank",rel:"noopener noreferrer"},ce=e("ES2015+"),ve=e("\u3001"),ue={href:"https://staging-cn.vuejs.org/guide/introduction.html",target:"_blank",rel:"noopener noreferrer"},_e=e("vue3"),he=e("\u3001"),me={href:"https://pinia.vuejs.org/introduction.html",target:"_blank",rel:"noopener noreferrer"},pe=e("pinia"),be=e("\u3001"),ge={href:"https://router.vuejs.org/",target:"_blank",rel:"noopener noreferrer"},fe=e("vue-router"),ke=e(" \u3001"),je={href:"https://vitejs.cn/",target:"_blank",rel:"noopener noreferrer"},xe=e("vite2"),Ve=e(" \u3001"),Se={href:"http://www.axios-js.com/zh-cn/docs/",target:"_blank",rel:"noopener noreferrer"},we=e("axios"),Ce=e(" \u548C "),Ie={href:"https://element-plus.gitee.io/zh-CN/guide/design.html",target:"_blank",rel:"noopener noreferrer"},Pe=e("element-plus"),ye=e(" \u7B49\u6280\u672F\u3002"),Ee=e("\u6240\u6709\u7684\u8BF7\u6C42\u6570\u636E\u90FD\u4F7F\u7528 "),Ae={href:"http://mockjs.com/",target:"_blank",rel:"noopener noreferrer"},Ne=e("Mock.js"),Me=e(" \u8FDB\u884C\u6A21\u62DF\uFF0C\u63D0\u524D\u4E86\u89E3\u548C\u5B66\u4E60\u8FD9\u4E9B\u77E5\u8BC6\u4F1A\u5BF9\u4F7F\u7528\u672C\u9879\u76EE\u6709\u5F88\u5927\u7684\u5E2E\u52A9\u3002"),Re=t(`<h2 id="\u529F\u80FD" tabindex="-1"><a class="header-anchor" href="#\u529F\u80FD" aria-hidden="true">#</a> \u529F\u80FD</h2><div class="language-text ext-text line-numbers-mode"><pre class="language-text"><code>- \u767B\u5F55 / \u6CE8\u9500
+
+- \u6743\u9650\u9A8C\u8BC1
+  - \u9875\u9762\u6743\u9650
+  - \u6307\u4EE4\u6743\u9650
+  - \u6743\u9650\u914D\u7F6E
+  - \u4E8C\u6B65\u767B\u5F55
+
+- \u591A\u73AF\u5883\u53D1\u5E03
+  - dev sit stage prod
+
+- \u5168\u5C40\u529F\u80FD
+  - \u56FD\u9645\u5316\u591A\u8BED\u8A00
+  - \u591A\u79CD\u52A8\u6001\u6362\u80A4
+  - \u52A8\u6001\u4FA7\u8FB9\u680F\uFF08\u652F\u6301\u591A\u7EA7\u8DEF\u7531\u5D4C\u5957\uFF09
+  - \u52A8\u6001\u9762\u5305\u5C51
+  - \u5FEB\u6377\u5BFC\u822A(\u6807\u7B7E\u9875)
+  - Svg Sprite \u56FE\u6807
+  - \u672C\u5730/\u540E\u7AEF mock \u6570\u636E
+  - Screenfull\u5168\u5C4F
+  - \u81EA\u9002\u5E94\u6536\u7F29\u4FA7\u8FB9\u680F
+
+- \u7F16\u8F91\u5668
+  - \u5BCC\u6587\u672C
+  - Markdown
+  - JSON \u7B49\u591A\u683C\u5F0F
+
+- Excel
+  - \u5BFC\u51FAexcel
+  - \u5BFC\u5165excel
+  - \u524D\u7AEF\u53EF\u89C6\u5316excel
+  - \u5BFC\u51FAzip
+
+- \u8868\u683C
+  - \u52A8\u6001\u8868\u683C
+  - \u62D6\u62FD\u8868\u683C
+  - \u5185\u8054\u7F16\u8F91
+
+- \u9519\u8BEF\u9875\u9762
+  - 401
+  - 404
+
+- \u7D44\u4EF6
+  - \u5934\u50CF\u4E0A\u4F20
+  - \u8FD4\u56DE\u9876\u90E8
+  - \u62D6\u62FDDialog
+  - \u62D6\u62FDSelect
+  - \u62D6\u62FD\u770B\u677F
+  - \u5217\u8868\u62D6\u62FD
+  - SplitPane
+  - Dropzone
+  - Sticky
+  - CountTo
+
+- \u7EFC\u5408\u5B9E\u4F8B
+- \u9519\u8BEF\u65E5\u5FD7
+- Dashboard
+- \u5F15\u5BFC\u9875
+- ECharts \u56FE\u8868
+- Clipboard(\u526A\u8D34\u590D\u5236)
+- Markdown2html
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><h2 id="\u76EE\u5F55\u7ED3\u6784" tabindex="-1"><a class="header-anchor" href="#\u76EE\u5F55\u7ED3\u6784" aria-hidden="true">#</a> \u76EE\u5F55\u7ED3\u6784</h2><p>\u672C\u9879\u76EE\u5DF2\u7ECF\u4E3A\u4F60\u751F\u6210\u4E86\u4E00\u4E2A\u5B8C\u6574\u7684\u5F00\u53D1\u6846\u67B6\uFF0C\u63D0\u4F9B\u4E86\u6DB5\u76D6\u4E2D\u540E\u53F0\u5F00\u53D1\u7684\u5404\u7C7B\u529F\u80FD\u548C\u5751\u4F4D\uFF0C\u4E0B\u9762\u662F\u6574\u4E2A\u9879\u76EE\u7684\u76EE\u5F55\u7ED3\u6784\u3002</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>\u251C\u2500\u2500 mock                       <span class="token comment"># \u9879\u76EEmock \u6A21\u62DF\u6570\u636E</span>
+\u251C\u2500\u2500 public                     <span class="token comment"># \u9759\u6001\u8D44\u6E90</span>
+\u2502   \u2502\u2500\u2500 favicon.ico            <span class="token comment"># favicon\u56FE\u6807</span>
+\u251C\u2500\u2500 src                        <span class="token comment"># \u6E90\u4EE3\u7801</span>
+\u2502   \u251C\u2500\u2500 api                    <span class="token comment"># \u6240\u6709\u8BF7\u6C42</span>
+\u2502   \u251C\u2500\u2500 assets                 <span class="token comment"># \u4E3B\u9898 \u5B57\u4F53\u7B49\u9759\u6001\u8D44\u6E90</span>
+\u2502   \u251C\u2500\u2500 components             <span class="token comment"># \u5168\u5C40\u516C\u7528\u7EC4\u4EF6</span>
+\u2502   \u251C\u2500\u2500 directive              <span class="token comment"># \u5168\u5C40\u6307\u4EE4</span>
+\u2502   \u251C\u2500\u2500 layout                 <span class="token comment"># \u5168\u5C40 layout</span>
+\u2502   \u251C\u2500\u2500 router                 <span class="token comment"># \u8DEF\u7531</span>
+\u2502   \u251C\u2500\u2500 store                  <span class="token comment"># \u5168\u5C40 store\u7BA1\u7406</span>
+\u2502   \u251C\u2500\u2500 styles                 <span class="token comment"># \u5168\u5C40\u6837\u5F0F</span>
+\u2502   \u251C\u2500\u2500 utils                  <span class="token comment"># \u5168\u5C40\u516C\u7528\u65B9\u6CD5</span>
+\u2502   \u251C\u2500\u2500 views                  <span class="token comment"># views \u6240\u6709\u9875\u9762</span>
+\u2502   \u251C\u2500\u2500 App.vue                <span class="token comment"># \u5165\u53E3\u9875\u9762</span>
+\u2502   \u251C\u2500\u2500 main.js                <span class="token comment"># \u5165\u53E3\u6587\u4EF6 \u52A0\u8F7D\u7EC4\u4EF6 \u521D\u59CB\u5316\u7B49</span>
+\u251C\u2500\u2500 .env.xxx                   <span class="token comment"># \u73AF\u5883\u53D8\u91CF\u914D\u7F6E</span>
+\u251C\u2500\u2500 .eslintrc.js               <span class="token comment"># eslint \u914D\u7F6E\u9879</span>
+\u251C\u2500\u2500 index.html                 <span class="token comment"># html\u6A21\u677F</span>
+\u251C\u2500\u2500 vite.config.js             <span class="token comment"># vite \u914D\u7F6E</span>
+\u251C\u2500\u2500 postcss.config.js          <span class="token comment"># postcss \u914D\u7F6E</span>
+\u251C\u2500\u2500 tailwind.config.js         <span class="token comment"># tailwind \u914D\u7F6E </span>
+\u2514\u2500\u2500 package.json               <span class="token comment"># package.json</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u5B89\u88C5" tabindex="-1"><a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a> \u5B89\u88C5</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token comment"># \u514B\u9686\u9879\u76EE</span>
+<span class="token function">git</span> clone https://github.com/mao-118/fast-vue-admin
+
+<span class="token comment"># \u8FDB\u5165\u9879\u76EE\u76EE\u5F55</span>
+<span class="token builtin class-name">cd</span> fast-admin
+
+<span class="token comment"># \u5B89\u88C5\u4F9D\u8D56</span>
+<span class="token function">npm</span> <span class="token function">install</span>
+
+<span class="token comment"># \u901A\u8FC7\u5982\u4E0B\u64CD\u4F5C\u89E3\u51B3 npm \u4E0B\u8F7D\u901F\u5EA6\u6162\u7684\u95EE\u9898</span>
+<span class="token function">npm</span> <span class="token function">install</span> --registry<span class="token operator">=</span>https://registry.npm.taobao.org
+
+<span class="token comment"># \u672C\u5730\u5F00\u53D1 \u542F\u52A8\u9879\u76EE</span>
+<span class="token function">npm</span> run dev
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><br><div class="custom-container tip"><p class="custom-container-title">TIP</p><p>\u529F\u80FD\u8FD8\u5728\u5B8C\u5584</p></div><br>`,11),ze=e("\u542F\u52A8\u5B8C\u6210\u540E\u4F1A\u81EA\u52A8\u6253\u5F00\u6D4F\u89C8\u5668\u8BBF\u95EE "),De={href:"http://localhost:9999",target:"_blank",rel:"noopener noreferrer"},Te=e("http://localhost:9999"),Be=e("\uFF0C \u4F60\u770B\u5230\u4E0B\u9762\u7684\u9875\u9762\u5C31\u4EE3\u8868\u64CD\u4F5C\u6210\u529F\u4E86\u3002"),Fe=t('<p><img src="'+v+'" alt="\u56FE\u7247"></p><p>\u63A5\u4E0B\u6765\u4F60\u53EF\u4EE5\u4FEE\u6539\u4EE3\u7801\u8FDB\u884C\u4E1A\u52A1\u5F00\u53D1\u4E86\uFF0C\u672C\u9879\u76EE\u5185\u5EFA\u4E86\u5178\u578B\u4E1A\u52A1\u6A21\u677F\u3001\u5E38\u7528\u4E1A\u52A1\u7EC4\u4EF6\u3001\u6A21\u62DF\u6570\u636E\u3001HMR \u5B9E\u65F6\u9884\u89C8\u3001\u72B6\u6001\u7BA1\u7406\u3001\u56FD\u9645\u5316\u3001\u5168\u5C40\u8DEF\u7531\u7B49\u7B49\u5404\u79CD\u5B9E\u7528\u7684\u529F\u80FD\u6765\u8F85\u52A9\u5F00\u53D1\uFF0C\u4F60\u53EF\u4EE5\u7EE7\u7EED\u9605\u8BFB\u548C\u63A2\u7D22\u5DE6\u4FA7\u7684\u5176\u5B83\u6587\u6863\u3002</p><br><h2 id="\u5176\u5B83" tabindex="-1"><a class="header-anchor" href="#\u5176\u5B83" aria-hidden="true">#</a> \u5176\u5B83</h2><p>\u53EF\u4EE5\u52A0\u5165 QQ\u7FA4\uFF08491864545\uFF09\u5206\u4EAB\u4E00\u4E9B\u6280\u672F\u76F8\u5173\u7684\u4E1C\u897F\u3002</p><h2 id="vue-\u751F\u6001\u5708" tabindex="-1"><a class="header-anchor" href="#vue-\u751F\u6001\u5708" aria-hidden="true">#</a> Vue \u751F\u6001\u5708</h2><p><strong>\u9996\u5148\u4E86\u89E3\u8FD9\u4E9B vue \u751F\u6001\u5708\u7684\u4E1C\u897F\uFF0C\u4F1A\u5BF9\u4F60\u4E0A\u624B\u672C\u9879\u76EE\u6709\u5F88\u5927\u7684\u5E2E\u52A9\u3002</strong></p>',7),He={href:"https://router.vuejs.org/",target:"_blank",rel:"noopener noreferrer"},Je=e("Vue Router"),Le=e(" \u662F vue \u5B98\u65B9\u7684\u8DEF\u7531\u3002\u5B83\u80FD\u5FEB\u901F\u7684\u5E2E\u52A9\u4F60\u6784\u5EFA\u4E00\u4E2A\u5355\u9875\u9762\u6216\u8005\u591A\u9875\u9762\u7684\u9879\u76EE\u3002"),Qe={href:"https://pinia.vuejs.org/introduction.html",target:"_blank",rel:"noopener noreferrer"},Oe=e("pinia"),qe=e(" \u6700\u521D\u662F\u5728 2019 \u5E74 11 \u6708\u5DE6\u53F3\u91CD\u65B0\u8BBE\u8BA1\u4F7F\u7528Composition API\u7684 Vue Store \u5916\u89C2\u7684\u5B9E\u9A8C\u3002\u4ECE\u90A3\u65F6\u8D77\uFF0C\u6700\u521D\u7684\u539F\u5219\u4ECD\u7136\u76F8\u540C\uFF0C\u4F46 Pinia \u9002\u7528\u4E8E Vue 2 \u548C Vue 3 \uFF0C\u5E76\u4E14\u4E0D\u9700\u8981\u4F60\u4F7F\u7528\u7EC4\u5408 API\u3002\u9664\u4E86\u5B89\u88C5\u548CSSR\u4E4B\u5916\uFF0C\u4E24\u8005\u7684 API \u90FD\u662F\u76F8\u540C\u7684\uFF0C\u5E76\u4E14\u8FD9\u4E9B\u6587\u6863\u9488\u5BF9 Vue 3 \uFF0C\u5E76\u5728\u5FC5\u8981\u65F6\u63D0\u4F9B\u6709\u5173 Vue 2 \u7684\u6CE8\u91CA\uFF0C\u4EE5\u4FBF Vue 2 \u548C Vue 3 \u7528\u6237\u53EF\u4EE5\u9605\u8BFB\uFF01"),Ge={href:"https://github.com/vuejs/vue-devtools",target:"_blank",rel:"noopener noreferrer"},Ke=e("Vue Dev-Tools"),Ue=e(" Vue \u5728\u6D4F\u89C8\u5668\u4E0B\u7684\u8C03\u8BD5\u5DE5\u5177\u3002\u5199 vue \u5FC5\u5907\u7684\u4E00\u4E2A\u6D4F\u89C8\u5668\u63D2\u4EF6\uFF0C\u80FD\u5927\u5927\u7684\u63D0\u9AD8\u4F60\u8C03\u8BD5\u7684\u6548\u7387\u3002"),We={href:"https://vitejs.cn/",target:"_blank",rel:"noopener noreferrer"},Xe=e("vite"),Ye=e(" \u662F\u4E00\u79CD\u65B0\u578B\u524D\u7AEF\u6784\u5EFA\u5DE5\u5177\uFF0C\u80FD\u591F\u663E\u8457\u63D0\u5347\u524D\u7AEF\u5F00\u53D1\u4F53\u9A8C\u3002\u5B83\u4E3B\u8981\u7531\u4E24\u90E8\u5206\u7EC4\u6210\uFF1A\u4E00\u4E2A\u5F00\u53D1\u670D\u52A1\u5668\uFF0C\u5B83\u57FA\u4E8E \u539F\u751F ES \u6A21\u5757 \u63D0\u4F9B\u4E86 \u4E30\u5BCC\u7684\u5185\u5EFA\u529F\u80FD\uFF0C\u5982\u901F\u5EA6\u5FEB\u5230\u60CA\u4EBA\u7684 \u6A21\u5757\u70ED\u66F4\u65B0\uFF08HMR\uFF09\u3002\u4E00\u5957\u6784\u5EFA\u6307\u4EE4\uFF0C\u5B83\u4F7F\u7528 Rollup \u6253\u5305\u4F60\u7684\u4EE3\u7801\uFF0C\u5E76\u4E14\u5B83\u662F\u9884\u914D\u7F6E\u7684\uFF0C\u53EF\u8F93\u51FA\u7528\u4E8E\u751F\u4EA7\u73AF\u5883\u7684\u9AD8\u5EA6\u4F18\u5316\u8FC7\u7684\u9759\u6001\u8D44\u6E90\u3002Vite \u610F\u5728\u63D0\u4F9B\u5F00\u7BB1\u5373\u7528\u7684\u914D\u7F6E\uFF0C\u540C\u65F6\u5B83\u7684 \u63D2\u4EF6 API \u548C JavaScript API \u5E26\u6765\u4E86\u9AD8\u5EA6\u7684\u53EF\u6269\u5C55\u6027\uFF0C\u5E76\u6709\u5B8C\u6574\u7684\u7C7B\u578B\u652F\u6301\u3002"),Ze={href:"https://github.com/vuejs/vetur",target:"_blank",rel:"noopener noreferrer"},$e=e("Vetur"),en=e(" \u662F VS Code \u7684\u63D2\u4EF6. \u5982\u679C\u4F60\u4F7F\u7528 VS Code \u6765\u5199 vue \u7684\u8BDD\uFF0C\u8FD9\u4E2A\u63D2\u4EF6\u662F\u5FC5\u4E0D\u53EF\u5C11\u7684\u3002");function nn(sn,tn){const s=r("ExternalLinkIcon");return a(),l(d,null,[_,n("p",null,[n("a",h,[m,i(s)]),p,b,n("a",g,[f,i(s)]),k,n("a",j,[x,i(s)]),V,n("a",S,[w,i(s)]),C,n("a",I,[P,i(s)]),y,n("a",E,[A,i(s)]),N,n("a",M,[R,i(s)]),z,n("a",D,[T,i(s)]),B,n("a",F,[H,i(s)])]),J,n("ul",null,[n("li",null,[L,n("a",Q,[O,i(s)]),q,n("a",G,[K,i(s)]),U]),n("li",null,[W,n("a",X,[Y,i(s)]),Z,n("a",$,[ee,i(s)]),ne,n("a",se,[ie,i(s)]),te,n("a",oe,[re,i(s)]),ae]),n("li",null,[le,n("a",de,[ce,i(s)]),ve,n("a",ue,[_e,i(s)]),he,n("a",me,[pe,i(s)]),be,n("a",ge,[fe,i(s)]),ke,n("a",je,[xe,i(s)]),Ve,n("a",Se,[we,i(s)]),Ce,n("a",Ie,[Pe,i(s)]),ye]),n("li",null,[Ee,n("a",Ae,[Ne,i(s)]),Me])]),Re,n("p",null,[ze,n("a",De,[Te,i(s)]),Be]),Fe,n("ol",null,[n("li",null,[n("p",null,[n("a",He,[Je,i(s)]),Le])]),n("li",null,[n("p",null,[n("a",Qe,[Oe,i(s)]),qe])]),n("li",null,[n("p",null,[n("a",Ge,[Ke,i(s)]),Ue])]),n("li",null,[n("p",null,[n("a",We,[Xe,i(s)]),Ye])]),n("li",null,[n("p",null,[n("a",Ze,[$e,i(s)]),en])])])],64)}var rn=o(u,[["render",nn],["__file","index.html.vue"]]);export{rn as default};
